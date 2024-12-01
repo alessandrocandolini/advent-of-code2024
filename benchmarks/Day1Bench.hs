fd56af05cb3d1e958d4923e4288fabb7b07e75fb
Alessandro Candolini <alessandrocandolini@gmail.com>
2024-12-01 17:34:30 +0000

    improve readability of benchmarks and create one for 1M

--- benchmarks/Day1Bench.hs
+++ benchmarks/Day1Bench.hs
@@ -1,3 +1,5 @@
+{-# LANGUAGE NumericUnderscores #-}
+
 module Day1Bench (benchmarks) where
 
 import Criterion.Main
@@ -6,12 +8,15 @@ import Day1 (part1, part2)
 generateInput :: Int -> [(Int, Int)]
 generateInput n = zip [1 .. n] (reverse [1 .. n])
 
+values :: [Int]
+values = [1000, 10_000, 100_000]
+
 benchmarks :: [Benchmark]
 benchmarks =
   [ bgroup
       "part1"
-      (fmap (\n -> bench (show n) $ whnf part1 (generateInput n)) [1000, 10000, 100000])
+      (fmap (\n -> bench (show n) $ whnf part1 (generateInput n)) values)
   , bgroup
       "part2"
-      (fmap (\n -> bench (show n) $ whnf part2 (generateInput n)) [1000, 10000, 100000])
+      (fmap (\n -> bench (show n) $ whnf part2 (generateInput n)) values)
   ]
