f50e34aff9ed0f113e1e5ab5c2602ad62225d6b0
Alessandro Candolini <alessandrocandolini@gmail.com>
2024-12-04 09:29:01 +0000

    Day 4 part 1 - missing diagonals

--- src/Day4.hs
+++ src/Day4.hs
@@ -1,11 +1,62 @@
 module Day4 where
+
+import Data.List (tails, transpose, inits)
 import qualified Data.Text as T
 import qualified Data.Text.IO as T
+import Witherable (mapMaybe)
 
 program :: FilePath -> IO ()
 program = (=<<) print . fmap logic . T.readFile
 
-data Answer = Answer deriving (Eq, Show)
+data Answer = Answer Int deriving (Eq, Show)
+
+answer :: Grid Char -> Answer
+answer = Answer . part1
 
 logic :: T.Text -> Answer
-logic = const Answer
+logic = answer . parseGrid
+
+part1 :: Grid Char -> Int
+part1 = countAll . findOccurrences parse
+
+data Xmas = Xmas | Samx deriving (Eq, Show)
+
+newtype Grid a = Grid
+  { grid :: [[a]]
+  }
+  deriving (Eq, Show)
+
+data Occurrences a = Occurrences
+  { horizontal :: [a]
+  , vertical :: [a]
+  , diagonal :: [a]
+  }
+  deriving (Eq, Show)
+
+countAll :: Occurrences a -> Int
+countAll o = length (horizontal o) + length (vertical o) + length (diagonal o)
+
+horizontalSlices :: Grid a -> [[a]]
+horizontalSlices = concatMap tails . grid
+
+verticalSlices :: Grid a -> [[a]]
+verticalSlices = horizontalSlices . Grid . transpose . grid
+
+diagonalSlices :: Grid a -> [[a]]
+diagonalSlices (Grid g) = [[]]
+
+
+findOccurrences :: ([a] -> Maybe b) -> Grid a -> Occurrences b
+findOccurrences p =
+  Occurrences
+    <$> (mapMaybe p . horizontalSlices)
+    <*> (mapMaybe p . verticalSlices)
+    <*> (mapMaybe p . diagonalSlices)
+
+parse :: String -> Maybe Xmas
+parse ('X' : 'M' : 'A' : 'S' : _) = Just Xmas
+parse ('S' : 'A' : 'M' : 'X' : _) = Just Samx
+parse _ = Nothing
+
+parseGrid :: T.Text -> Grid Char
+parseGrid = Grid . fmap T.unpack . T.lines
--- test/Day4Spec.hs
+++ test/Day4Spec.hs
@@ -1,21 +1,81 @@
+{-# LANGUAGE QuasiQuotes #-}
+
 module Day4Spec where
 
-import Day4
-import Test.Hspec
-import Test.Hspec.QuickCheck
-import Test.QuickCheck
+import Data.List (tails)
 import qualified Data.Text as T
 import qualified Data.Text.IO as T
+import Day4
+import NeatInterpolation (trimming)
+import SpecUtils (shouldBePretty)
+import Test.Hspec (Spec, describe, it, shouldBe, xit)
+import Test.Hspec.QuickCheck
+import Test.Hspec.QuickCheck ()
+import Test.QuickCheck
+import Test.QuickCheck ()
+
+example :: T.Text
+example =
+  [trimming|
+      MMMSXXMASM
+      MSAMXMSMSA
+      AMXSXMAAMM
+      MSAMASMSMX
+      XMASAMXAMM
+      XXAMMXXAMA
+      SMSMSASXSS
+      SAXAMASAAA
+      MAMMMXMMMM
+      MXMXAXMASX
+ |]
+
+slices :: Grid a -> [[a]]
+slices g = horizontalSlices g ++ verticalSlices g ++ diagonalSlices g
+
+findXmasOccurrences :: Grid Char -> Occurrences Xmas
+findXmasOccurrences = findOccurrences parse
 
 spec :: Spec
 spec = describe "Day 4" $ do
-  it ""
-    $ 1
-    `shouldBe` 1
+  it "slices on a single row" $
+    let
+      input :: String
+      input = "MMMSXXMASM"
+     in
+      slices (Grid [input]) `shouldBe` ["MMMSXXMASM", "MMSXXMASM", "MSXXMASM", "SXXMASM", "XXMASM", "XMASM", "MASM", "ASM", "SM", "M", "", "M", "", "M", "", "M", "", "S", "", "X", "", "X", "", "M", "", "A", "", "S", "", "M", "", ""]
 
-  prop ""
-    $ \l -> reverse (reverse l) == (l :: [Int])
+  it "horizontal and vertical slices" $
+    let
+      input :: Grid Char
+      input =
+        Grid
+          ( lines
+              ( T.unpack
+                  [trimming|
+                    MSXA
+                    MSAM
+                    AMXS
+                  |]
+              )
+          )
+      expected :: [[Char]]
+      expected = ["MSXA", "SXA", "XA", "A", "", "MSAM", "SAM", "AM", "M", "", "AMXS", "MXS", "XS", "S", "", "MMA", "MA", "A", "", "SSM", "SM", "M", "", "XAX", "AX", "X", "", "AMS", "MS", "S", ""]
+     in
+      (horizontalSlices input ++ verticalSlices input) `shouldBe` expected
 
-  xit "solve the puzzle" $ do
-     input <- T.readFile "resources/input4"
-     logic input `shouldBe` Answer
+  it "parse xmas" $
+    parse "XMASM" `shouldBe` Just Xmas
+
+  it "findOccurrences in one line" $
+    findXmasOccurrences (Grid ["MMMSXXMASM"]) `shouldBe` (Occurrences [Xmas] [] [])
+
+  it "findOccurrences in puzzle example" $
+    findXmasOccurrences (parseGrid example)
+      `shouldBe` Occurrences
+        { horizontal = [Xmas, Samx, Xmas, Samx, Xmas]
+        , vertical = [Samx, Xmas, Samx]
+        , diagonal = [Samx, Xmas, Samx, Samx, Samx, Samx, Xmas, Xmas, Xmas, Xmas]
+        }
+
+  it "part1" $
+    (part1 . parseGrid) example `shouldBe` 18
